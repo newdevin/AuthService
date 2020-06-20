431e60cf0507c4c13ef66edf1464c5b4936e24e8
Devinder Singh <newdevin@gmail.com>
2020-06-20 08:07:27 +0100

    fact attribute added

--- AuthService.Test/DomainTest.fs
+++ AuthService.Test/DomainTest.fs
@@ -10,6 +10,7 @@ let ``createSecretToken should return none `` () =
     let result = Domain.createSecretToken "" (DateTime.Now.Date) (DateTime.Now.Date.AddDays(2.))
     result |> should equal None
 
+[<Fact>]
 let ``createSecretToken should return secret `` () =
     let token = "token"
     let createdOn = DateTime.Now.Date
